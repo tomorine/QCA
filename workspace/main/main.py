6fbcccabec8d924729725da66a200b6e004dd201
tomorine <tomorow@ngc.is.ritsumei.ac.jp>
2019-05-14 22:44:13 +0900

    fix op adjust path function

--- workspace/main/main.py
+++ workspace/main/main.py
@@ -2,8 +2,6 @@
 # writing by python3 
 # coding:utf-8
 # Implies関数はバグの元. 極力If関数を使うべし
-
-
 # インポートz3
 from z3 import *  
 from circ import *
@@ -22,15 +20,15 @@ def main():
     circ = Make_Network.blif(str)
     Print_Network.node_inf(circ)
 
-    # 制約式の追加
     hi = 5 # circuit high
     wd = 6 # circuit wide
-    s = Solver()
-    
-    # op_exist is int variable : op_exist[op_id][wide][high]
-    # wire_exist is int variable : wire_exist[source_op][distination_op][wide][high]
-    # clock_zone is int variable : clock_zone[wide][high]
-    # path is int variable : if information flow exist as operaor[a][b] or wire[a][b] -> operator[c][d] or wire[c][d], path[a][b][c][d] is 1
+    s = Solver() # sは制約式の集合
+
+    # 以下制約式の追加
+    # op_exist is int variable : op_exist[op_id][wide][high]が存在するとき１になる
+    # wire_exist is int variable : wire_exist[source_op][distination_op][wide][high]が存在するとき１になる
+    # clock_zone is int variable : clock_zone[wide][high]のクロックゾーンを管理(1<=clock_zone<=4)
+    # path is int variable : path[a][b][c][d]が位置の時クロックゾーン[a][b]から[c][d]にデータフローが存在する
     # ic is int variable : operatorの周りのクロックゾーンにファンイン数（ファンアウト数）と同じ数だけ入力（出力）を確約する。
     # todo : wireがoperatorから離れてしまう問題(4/30)
     op_exist = [[[Int("op_exist[%d][%d][%d]" % (k,j,i)) for i in range(hi)] for j in range(wd)]for k in range(circ.op_num)]
@@ -51,16 +49,14 @@ def main():
                         s.add(0 <= connect[j][k][ir][jr], connect[j][k][ir][jr] <= 1)
                 for node in range(circ.op_num):
                     s.add(0 <= wire_exist[i][node][j][k], wire_exist[i][node][j][k] <= 1)
-
-    # op should be used only once
+    # すべてのopは一度必ず使わなければならない
     for i in range(circ.op_num):
         tmplist = []
         for j in range(wd):
             for k in range(hi):
                 tmplist.append(op_exist[i][j][k])
         s.add(Sum([tmp for tmp in tmplist]) == 1)
-
-    # clock zone can have only one oprater or one wire
+    # クロックゾーンには１つのopかwireしか存在できない制約
     for k in range(hi):
         for j in range(wd):
             tmplist = []
@@ -70,17 +66,13 @@ def main():
                     tmplist.append(wire_exist[id][i][j][k])
                 tmplist.append(op_exist[i][j][k])
             s.add(Sum([tmp for tmp in tmplist]) <= 1)
-
-    # adjacent clock zone has different number
+    # 隣接するクロックゾーンは違う数字でなければならない制約
     for i in range(wd):
         for j in range(hi):
             if i<wd-1:
                 s.add(clock_zone[i][j]!=clock_zone[i+1][j])
             if j<hi-1:
                 s.add(clock_zone[i][j]!=clock_zone[i][j+1])
-
-
-
     # operatorの隣接するクロックゾーンにファンイン（ファンアウト）数と同じ数のconnectを定義する制約
     # 同時にopかwireを定義する定義する制約
     for i in range(wd):
@@ -222,7 +214,6 @@ def main():
                         [tmp for tmp in doutlist])+Sum([tmp for tmp in uoutlist]) == 1, op_exist[node][i][j] == 0))
                 # s.add(If(op_exist[node][i][j] == 1, Sum([tmp for tmp in tmplist]) == len(circ.find_node_id(node).input),op_exist[node][i][j] == 0))
                 # s.add(Sum([tmp for tmp in rtmplist]) <= 1, Sum([tmp for tmp in ltmplist]) <= 1, Sum([tmp for tmp in dtmplist]) <= 1, Sum([tmp for tmp in utmplist]) <= 1)
-
     # wireはループしない制約
     # 0 <= path <= 1 
     for i in range(wd+1):
@@ -234,21 +225,37 @@ def main():
                     for irr in range(wd+1):
                         for jrr in range(hi+1):
                             s.add(Implies(And(path[i][j][ir][jr]==1, path[ir][jr][irr][jrr]==1),path[i][j][irr][jrr]==1))
-
     # connectが存在する所にpathを通す制約
     for i in range(wd):
         for j in range(hi):
             for ir in range(wd):
                 for jr in range(hi):
                     s.add(Implies(connect[i][j][ir][jr]==1, path[i][j][ir][jr]==1))
-
-
     # wireが途切れないようにする制約
     # todo : 色々とおかしいかも
     for i in range(wd):
         for j in range(hi):
             for node in range(circ.op_num):
-
+                for sonode in circ.find_node_id(node).input:
+                    id = sonode.id
+                    tmplist = []
+                    if i < wd-1:
+                        tmplist.append(op_exist[node][i+1][j])
+                        tmplist.append(op_exist[id][i+1][j])
+                        tmplist.append(wire_exist[id][node][i+1][j])
+                    if i > 0:
+                        tmplist.append(op_exist[node][i-1][j])
+                        tmplist.append(op_exist[id][i-1][j])
+                        tmplist.append(wire_exist[id][node][i-1][j])
+                    if j < hi-1:
+                        tmplist.append(op_exist[node][i][j+1])
+                        tmplist.append(op_exist[id][i][j+1])
+                        tmplist.append(wire_exist[id][node][i][j+1])
+                    if j > 0:
+                        tmplist.append(op_exist[node][i][j-1])
+                        tmplist.append(op_exist[id][i][j-1])
+                        tmplist.append(wire_exist[id][node][i][j-1])
+                    s.add(Implies(wire_exist[id][node][i][j]==1, Sum([tmp for tmp in tmplist]) == 2))
     """
     for j in range(wd):
         for k in range(hi):
@@ -295,19 +302,25 @@ def main():
     # wireが合流しないようにする制約
     for i in range(wd):
         for j in range(hi):
-                tmplist = []
+                intmplist = []
+                outtmplist = []
                 if i<wd-1:
-                    tmplist.append(path[i+1][j][i][j])
+                    intmplist.append(path[i+1][j][i][j])
+                    outtmplist.append(path[i][j][i+1][j])
                 if i>0:
-                    tmplist.append(path[i-1][j][i][j])
+                    intmplist.append(path[i-1][j][i][j])
+                    outtmplist.append(path[i][j][i-1][j])
                 if j<hi - 1:
-                    tmplist.append(path[i][j+1][i][j])
+                    intmplist.append(path[i][j+1][i][j])
+                    outtmplist.append(path[i][j][i][j+1])
                 if j>0:
-                    tmplist.append(path[i][j-1][i][j])
+                    intmplist.append(path[i][j-1][i][j])
+                    outtmplist.append(path[i][j][i][j-1])
                 for node in range(circ.op_num):
                     for sonode in circ.find_node_id(node).input:
                         id = sonode.id
-                        s.add(Implies(wire_exist[id][node][i][j]==1, Sum([tmp for tmp in tmplist])<=1))
+                        s.add(Implies(wire_exist[id][node][i][j] == 1, Sum([tmp for tmp in intmplist]) <= 1))
+                        s.add(Implies(wire_exist[id][node][i][j] == 1, Sum([tmp for tmp in outtmplist]) <= 1))
     # wireがあった場合sourceのopにつながっているようにする制約
     for i in range(wd):
         for j in range(hi):
@@ -400,7 +413,6 @@ def main():
                                                      And(clock_zone[i][j] == 3, clock_zone[i][j-1] == 4),
                                                      And(clock_zone[i][j] == 4, clock_zone[i][j-1] == 1)),
                                                      path[i][j][i][j-1] == 0))
-
     # pathが存在するところにはopかwireが存在するという制約
 
     # print model or
